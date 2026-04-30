a87aa8037492361fe08102faffb67ab0cefa02a5
Pierre Wulles <pierre.wulles@gmail.com>
2026-04-30 12:54:53 +0200

    Remove useless trait in qr faer

--- mdarray-linalg-faer/src/qr/context.rs
+++ mdarray-linalg-faer/src/qr/context.rs
@@ -16,11 +16,7 @@ use crate::Faer;
 
 impl<T, D0: Dim, D1: Dim> QR<T, D0, D1> for Faer
 where
-    T: ComplexFloat
-        + ComplexField
-        + Default
-        + std::convert::From<<T as num_complex::ComplexFloat>::Real>
-        + 'static,
+    T: ComplexFloat + ComplexField + Default + 'static,
 {
     /// Compute full QR decomposition with new allocated matrices
     fn qr<L: Layout>(
